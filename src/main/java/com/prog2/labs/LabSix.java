5fdf884020413f3b29161dd1670900f502e8d2a2
AmirGeorgesHaya <129766673+AmirGeorgesHaya@users.noreply.github.com>
2024-02-05 17:31:02 -0500

    Update LabSix.java
    version 1.0

--- src/main/java/com/prog2/labs/LabSix.java
+++ src/main/java/com/prog2/labs/LabSix.java
@@ -1,4 +1,7 @@
 package com.prog2.labs;
+
+import java.util.ArrayList;
+
 /**
  * @author adinashby
  *
@@ -12,13 +15,22 @@ public class LabSix {
 	 */
 	public static void main(String[] args) {
 		
+            String input = "41 77 74 22 44\n 12\n 37 34 36 52\n \n 20 22 33";
+            
+            arrayListProblem(input);
 	}
 
 	/**
 	 * Please refer to the README file for question(s) description
 	 */
 
-	public String arrayListProblem(String input) {
+	public static String arrayListProblem(String input) {
+            ArrayList<ArrayList<String>> numbers = new ArrayList<>();
+            
+            numbers.add
+            
+            
+            
 		return "";
 	}
 }
